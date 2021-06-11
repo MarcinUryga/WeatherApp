cfc96e0884c7879b309f864a096ef9085d24ff82
Marcin Uryga <marcin.uryga@comarch.pl>
2021-06-11 09:33:54 +0200

    Make Temperature as Serializable

--- app/src/main/java/com/example/weatherapp/model/local/Temperature.kt
+++ app/src/main/java/com/example/weatherapp/model/local/Temperature.kt
@@ -1,9 +1,11 @@
 package com.example.weatherapp.model.local
 
+import java.io.Serializable
+
 class Temperature(
     var value: Double?,
     var degree: Degree
-) {
+) : Serializable {
 
     override fun toString() = "$value ${degree.mark}"
 
